880cebceadbfab369d21525f2efd34e6ba9c1f57
lingfeng <2090590980@qq.com>
2026-05-05 22:49:25 +0800

    009:添加了knife4j接口文档自动化框架,设计了退出登录,查询买到的订单，查询卖出的订单，删除订单，根据热度首页推荐商品,重置密码，用户注册接口,添加了相关接口的DTO请求类,添加了拦截器的过滤url,order类添加了list<orderItem>属性,接口经knife4j调试后测试通过

---
+++ src/main/java/com/lingfeng/secondhandtradingplatform/DTO/RegisterRequest.java
@@ -0,0 +1,39 @@
+package com.lingfeng.secondhandtradingplatform.DTO;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Schema(description = "用户注册请求")
+public class RegisterRequest {
+
+    @NotBlank(message = "手机号不能为空")
+    @Schema(description = "手机号", requiredMode = Schema.RequiredMode.REQUIRED, example = "13800138000")
+    @Pattern(regexp = "^1[3-9]\\d{9}$", message = "手机号格式不正确")
+    private String phone;
+
+    @NotBlank(message = "验证码不能为空")
+    @Schema(description = "验证码", requiredMode = Schema.RequiredMode.REQUIRED, example = "123456")
+    private String code;
+
+    @Schema(description = "用户名", example = "张三")
+    @Pattern(regexp = "^[a-zA-Z0-9\u4e00-\u9fa5]{2,20}$",
+            message = "用户名只能包含中文、英文、数字，长度2-20位")
+    private String username;
+
+    @Schema(description = "性别", example = "男")
+    private String sex;
+
+    @NotBlank(message = "密码不能为空")
+    @Size(min = 6, max = 20, message = "密码长度6-20位")
+    @Schema(description = "密码", requiredMode = Schema.RequiredMode.REQUIRED, example = "123456")
+    private String password;
+
+    @Schema(description = "头像", example = "/img/avatar.png")
+    private String img;
+}
---
+++ src/main/java/com/lingfeng/secondhandtradingplatform/DTO/ResetPasswordRequest.java
@@ -0,0 +1,28 @@
+package com.lingfeng.secondhandtradingplatform.DTO;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@Schema(description = "重置密码请求")
+public class ResetPasswordRequest {
+
+    @Schema(description = "手机号", requiredMode = Schema.RequiredMode.REQUIRED, example = "13800138000")
+    @NotBlank(message = "手机号不能为空")
+    @Pattern(regexp = "^1[3-9]\\d{9}$", message = "手机号格式不正确")
+    private String phone;
+
+    @Schema(description = "新密码", requiredMode = Schema.RequiredMode.REQUIRED, example = "123456")
+    @NotBlank(message = "新密码不能为空")
+    @Size(min = 6, max = 20, message = "密码长度6-20位")
+    private String newPassword;
+
+    @Schema(description = "验证码", requiredMode = Schema.RequiredMode.REQUIRED, example = "123456")
+    @NotBlank(message = "验证码不能为空")
+    private String code;
+}
